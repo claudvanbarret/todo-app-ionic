cc52e8fb707ff9d9f0368477ffe133bb26258636
Silva <claudivan.b.silva@accenture.com>
2018-05-28 11:47:06 -0300

    feat: add user into the firestore

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,26 +2,43 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import AuthProvider = firebase.auth.AuthProvider;
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { User } from '../models/user.model';
 
 @Injectable()
 export class AuthService {
-	private user: firebase.User;
+	private user: User;
 
-	constructor(public afAuth: AngularFireAuth) {
+	constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) {
 		afAuth.authState.subscribe(user => {
-			this.user = user;
+			if(user) {
+				this.updateUserData(user);
+			}	
 		});
 	}
 
 	signInWithEmail(credentials) {
 		return this.afAuth.auth.signInWithEmailAndPassword(credentials.email, credentials.password);
-    }
+  }
     
-    signUp(credentials){
-        return this.afAuth.auth.createUserWithEmailAndPassword(credentials.email, credentials.password);
+  signUp(credentials){
+    return this.afAuth.auth.createUserWithEmailAndPassword(credentials.email, credentials.password);
 	}
 	
 	singOut(){
 		return this.afAuth.auth.signOut();
+	}
+
+	private updateUserData(user){
+		const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+
+		const data: User = {
+			uid: user.uid,
+			email: user.email,
+			photoURL: user.photoURL,
+			displayName: user.displayName
+		}
+
+		return userRef.set(data);
 	}
 }
